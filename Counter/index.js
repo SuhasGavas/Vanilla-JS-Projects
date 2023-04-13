12d47e4833b850da73f6a2d8a8ce227299641906
Suhas Vasudev Gavas (BD/PLP5) <SuhasVasudev.Gavas@in.bosch.com>
2023-04-13 11:07:31 +0530

    Counter update

--- Counter/index.js
+++ Counter/index.js
@@ -1,21 +1,21 @@
 let counter = 0;
 
-const cntr = document.getElementById('cntr');
-const btn_incr = document.getElementById('btn_incr');
-const btn_decr = document.getElementById('btn_decr');
-const btn_reset = document.getElementById('btn_reset');
+const cntr = document.getElementById("cntr");
+const btn_incr = document.getElementById("btn_incr");
+const btn_decr = document.getElementById("btn_decr");
+const btn_reset = document.getElementById("btn_reset");
 
-btn_incr.addEventListener('click',()=>{
-    counter++;
-    cntr.innerHTML = counter;
-})
+btn_incr.addEventListener("click", () => {
+  counter++;
+  cntr.innerHTML = counter;
+});
 
-btn_decr.addEventListener('click',()=>{
-    counter--;
-    cntr.innerHTML = counter;
-})
+btn_decr.addEventListener("click", () => {
+  counter--;
+  cntr.innerHTML = counter;
+});
 
-btn_reset.addEventListener('click',()=>{
-    counter = 0;
-    cntr.innerHTML = counter;
-})
+btn_reset.addEventListener("click", () => {
+  counter = 0;
+  cntr.innerHTML = counter;
+});
